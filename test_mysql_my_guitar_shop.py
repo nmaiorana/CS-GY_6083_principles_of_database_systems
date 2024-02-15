55c832a792bccb91fe35fbac35cbb1f6b5e68b6c
Nicola Maiorana <nmaiorana@gmail.com>
2024-02-14 21:56:56 -0500

    Added MYSql DB

--- test_mysql_my_guitar_shop.py
+++ test_mysql_my_guitar_shop.py
@@ -7,10 +7,10 @@ cnx = None
 try:
     cnx = mysql.connector.connect(user='root', password=MYSQL_PW, host='localhost', database='my_guitar_shop')
     cursor = cnx.cursor()
-    query = ('select product_Id, product_name from products')
+    query = 'select product_Id, product_name, category_id from products'
     cursor.execute(query)
-    for (product_id, product_name) in cursor:
-        print(f'ID: {product_id} NAME: {product_name}')
+    for (product_id, product_name, category_id) in cursor:
+        print(f'ID: {product_id} NAME: {product_name} CAT: {category_id}')
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
